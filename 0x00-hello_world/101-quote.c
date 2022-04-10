0e53d1040db54ad7da37f07db4bd2e63ac99c27d
darnnie21 <danielchuks082@gmail.com>
2022-04-10 16:05:05 -0700

    Adding 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
@@ -10,6 +10,6 @@ int main(void)
 {
 	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, str1, 59);
+	fwrite(2, str1, 59);
 	return (1);
 }
